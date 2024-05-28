835fdc18b53c3738262b8d0f5b6ede6e157d138b
Nathan Genaudeau <nathan.genaudeau@gmail.com>
2024-05-28 20:44:53 +0200

    Add Vuetify for style and replace PolarAreaChart by BarChart

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-import { PolarAreaChart } from 'vue-chart-3';
+import { BarChart } from 'vue-chart-3';
 import { Chart, registerables } from 'chart.js';
 Chart.register(...registerables);
 
@@ -9,6 +9,19 @@ const randomRider: any = ref('');
 const guesses: any = ref([]);
 const input = ref('');
 const lifes = ref(10);
+
+const headers = [
+  { title: 'Nom', key: 'name', sortable: false },
+  { title: 'Age', key: 'age', sortable: false },
+  { title: 'Equipe', key: 'team', sortable: false },
+  { title: 'Nationalité', key: 'nationality', sortable: false },
+  { title: 'Taille', key: 'height', sortable: false },
+  { title: 'Poids', key: 'weight', sortable: false },
+  { title: 'Victoire', key: 'win', sortable: false },
+  { title: 'Participation GT', key: 'gt_participation', sortable: false },
+  { title: 'Participation classique', key: 'classic_participation', sortable: false },
+  { title: 'Stats', key: 'stats', sortable: false }
+];
 
 const props = defineProps({
   mode: String
@@ -47,22 +60,22 @@ const selectRider = (rider: any) => {
   lifes.value = rider === randomRider.value ? lifes.value : lifes.value - 1;
 };
 
-const config = {
-  responsive: true
-  // remove arc legend
+const getColor = (value: number) => {
+  if (value !== randomRider.value.age) return 'red';
+  return 'green';
+};
+
+const getArrow = (value: number) => {
+  if (value < randomRider.value.age) return 'mdi-chevron-up';
+  if (value > randomRider.value.age) return 'mdi-chevron-down';
+  return 'mdi-check';
 };
 
 const formatRiderSpecialities = (rider: any) => {
   return {
-    /*labels: [
-      'Classement général',
-      'Montagne',
-      'Sprint',
-      'Course d\'un jour',
-      'Contre la montre'
-    ],*/
+    labels: ['GC', 'MON', 'SPR', 'CLA', 'CLM'],
     datasets: [{
-      label: 'Spécialités',
+      label: ['Spécialités'],
       data: [rider.general_classification, rider.climber, rider.sprint, rider.one_day_races, rider.time_trial],
       backgroundColor: [
         'rgb(255, 99, 132)',
@@ -92,86 +105,14 @@ const formatRiderSpecialities = (rider: any) => {
     <span class="heart-empty" v-for="i in 10 - lifes" :key="i" />
   </div>
 
-  <table>
-    <tr>
-      <th>Nom</th>
-      <th>Âge</th>
-      <th>Équipe</th>
-      <th>Nationalité</th>
-      <th>Poids</th>
-      <th>Taille</th>
-      <th>Victoire</th>
-      <th>Part. GT</th>
-      <th>Part. Classique</th>
-      <!--<th>GC</th>
-      <th>Montagne</th>
-      <th>Sprint</th>
-      <th>Course d'un jour</th>
-      <th>Contre la montre</th>-->
-      <th>Graphe</th>
-    </tr>
-    <tr v-for="rider in guesses" :key="rider.id">
-      <td>{{ rider.name }}</td>
-      <td :class="rider.age === randomRider.age ? 'green' : 'red'">
-        {{ rider.age }} ans
-        <span v-if="rider.age < randomRider.age">&uarr;</span>
-        <span v-if="rider.age > randomRider.age">&darr;</span>
-      </td>
-      <td :class="rider.team === randomRider.team ? 'green' : 'red'">{{ rider.team }}</td>
-      <td :class="rider.nationality === randomRider.nationality ? 'green' : 'red'">{{ rider.nationality }}</td>
-      <td :class="rider.weight === randomRider.weight ? 'green' : 'red'">
-        {{ rider.weight }} kg
-        <span v-if="rider.weight < randomRider.weight">&uarr;</span>
-        <span v-if="rider.weight > randomRider.weight">&darr;</span>
-      </td>
-      <td :class="rider.height === randomRider.height ? 'green' : 'red'">
-        {{ rider.height }} m
-        <span v-if="rider.height < randomRider.height">&uarr;</span>
-        <span v-if="rider.height > randomRider.height">&darr;</span>
-      </td>
-      <td :class="rider.win === randomRider.win ? 'green' : 'red'">
-        {{ rider.win }}
-        <span v-if="rider.win < randomRider.win">&uarr;</span>
-        <span v-if="rider.win > randomRider.win">&darr;</span>
-      </td>
-      <td :class="rider.gt_participation === randomRider.gt_participation ? 'green' : 'red'">
-        {{ rider.gt_participation }}
-        <span v-if="rider.gt_participation < randomRider.gt_participation">&uarr;</span>
-        <span v-if="rider.gt_participation > randomRider.gt_participation">&darr;</span>
-      </td>
-      <td :class="rider.classic_participation === randomRider.classic_participation ? 'green' : 'red'">
-        {{ rider.classic_participation }}
-        <span v-if="rider.classic_participation < randomRider.classic_participation">&uarr;</span>
-        <span v-if="rider.classic_participation > randomRider.classic_participation">&darr;</span>
-      </td>
-      <!--<td :class="rider.general_classification === randomRider.general_classification ? 'green' : 'red'">
-        {{ rider.general_classification }}%
-        <span v-if="rider.general_classification < randomRider.general_classification">&uarr;</span>
-        <span v-if="rider.general_classification > randomRider.general_classification">&darr;</span>
-      </td>
-      <td :class="rider.climber === randomRider.climber ? 'green' : 'red'">
-        {{ rider.climber }}%
-        <span v-if="rider.climber < randomRider.climber">&uarr;</span>
-        <span v-if="rider.climber > randomRider.climber">&darr;</span>
-      </td>
-      <td :class="rider.sprint === randomRider.sprint ? 'green' : 'red'">
-        {{ rider.sprint }}%
-        <span v-if="rider.sprint < randomRider.sprint">&uarr;</span>
-        <span v-if="rider.sprint > randomRider.sprint">&darr;</span>
-      </td>
-      <td :class="rider.one_day_races === randomRider.one_day_races ? 'green' : 'red'">
-        {{ rider.one_day_races }}%
-        <span v-if="rider.one_day_races < randomRider.one_day_races">&uarr;</span>
-        <span v-if="rider.one_day_races > randomRider.one_day_races">&darr;</span>
-      </td>
-      <td :class="rider.time_trial === randomRider.time_trial ? 'green' : 'red'">
-        {{ rider.time_trial }}%
-        <span v-if="rider.time_trial < randomRider.time_trial">&uarr;</span>
-        <span v-if="rider.time_trial > randomRider.time_trial">&darr;</span>
-      </td>-->
-      <td>
-        <PolarAreaChart :chartData="formatRiderSpecialities(rider)" :config="config" />
-      </td>
-    </tr>
-  </table>
+  <v-data-table :items="guesses" :headers="headers">
+    <template v-slot:item.age="{ value }">
+      <v-chip :color=getColor(value)>
+        {{ value }} <v-icon :icon="getArrow(value)"/>
+      </v-chip>
+    </template>
+    <template v-slot:item.stats="{ item }">
+      <BarChart :chartData="formatRiderSpecialities(item)" />
+    </template>
+  </v-data-table>
 </template>
